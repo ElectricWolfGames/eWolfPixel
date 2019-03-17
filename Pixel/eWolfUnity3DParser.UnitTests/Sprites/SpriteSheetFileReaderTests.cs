cce0b77b5f6db5c5b2260a732a590f5e37b81e30
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2019-03-17 09:22:48 +0000

    Added some extra basic tests for SpriteSheetFileReader

---
+++ Pixel/eWolfUnity3DParser.UnitTests/Sprites/SpriteSheetFileReaderTests.cs
@@ -0,0 +1,19 @@
+﻿using eWolfUnity3DParser.Sprites;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace eWolfUnity3DParser.UnitTests.Sprites
+{
+    public class SpriteSheetFileReaderTests
+    {
+        [Test]
+        public void ShouldReadLines()
+        {
+            string rawFile = "a\nb\nc";
+            SpriteSheetFileReader spriteSheetFileReader = new SpriteSheetFileReader(rawFile);
+            spriteSheetFileReader.ReadLine().Should().Be("a");
+            spriteSheetFileReader.ReadLine().Should().Be("b");
+            spriteSheetFileReader.ReadLine().Should().Be("c");
+        }
+    }
+}
